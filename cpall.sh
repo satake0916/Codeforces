3704cff7d18ea249a8a52a1455e11891b9cf4769
satake0916 <stk_halfmoon@icloud.com>
2022-09-03 10:45:53 +0900

    add cpall.sh

---
+++ cpall.sh
@@ -0,0 +1,4 @@
+cp t.cpp A.cpp
+cp t.cpp B.cpp
+cp t.cpp C.cpp
+cp t.cpp D.cpp
--- t.cpp
+++ t.cpp
@@ -1,26 +1,4 @@
-#include<iostream>
-#include<string>
-#include<cstdio>
-#include<vector>
-#include<cmath>
-#include<algorithm>
-#include<functional>
-#include<iomanip>
-#include<queue>
-#include<ciso646>
-#include<random>
-#include<map>
-#include<set>
-#include<bitset>
-#include<stack>
-#include<unordered_map>
-#include<unordered_set>
-#include<utility>
-#include<cassert>
-#include<complex>
-#include<numeric>
-#include<array>
-#include<chrono>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define pb push_back
@@ -28,5 +6,14 @@ using ll = long long;
 #define fi first
 #define se second
 
+void solve(){
+}
+
 int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int T;
+	cin >> T;
+	while(T--) solve();
+	return 0;
 }
