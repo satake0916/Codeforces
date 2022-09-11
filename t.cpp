d0a41f72185701584f4f4f179df07191dd4c7a26
satake <stk_halfmoon@icloud.com>
2022-09-11 15:50:06 +0900

    debug.hを追加

--- t.cpp
+++ t.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define pb push_back
-#define eb emplace_back
-#define fi first
-#define se second
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 710
+#endif
+
 
 void solve(){
 }
